aa5adbb086f869622453bea3257b4f72f11b015d
Andy Radulescu <radulescu.eduard.andrei@gmail.com>
2018-03-26 12:09:28 +0300

    added POST todos and request router

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/Todo');
+var { User } = require('./models/User');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    let todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }).catch((err) => res.status(400).send(err));
+});
+
+app.listen(3000, () => {
+    console.log('Started on port 3000');
+});
